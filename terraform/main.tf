580fd1379ae9d89c2ec3b80d07edaeb970d38f82
sheriff suara <suara.sheriff@gmail.com>
2022-03-29 13:10:27 -0400

    update terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,7 +32,7 @@ resource "aws_s3_bucket_acl" "cl" {
 }
 
 resource "aws_s3_bucket_policy" "py" {
- bucket = aws_s3_bucket.bk.id
+  bucket = aws_s3_bucket.bk.id
   policy = data.aws_iam_policy_document.public.json
 }
 
