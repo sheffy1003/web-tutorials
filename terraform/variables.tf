8aa1df13c4c31e365be32757f4d1a1d2ceb5c9ae
sheriff suara <suara.sheriff@gmail.com>
2022-04-10 11:21:06 -0400

    test manual deployment

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,39 +16,39 @@ variable "config" {
   })))
   default = {
     tutorial = {
-    sandbox = {
-          acl    = "private"
-          bucket = "hl-sandbox-tutorial"
-          web = {
-              error_document = "index.html"
-              index_document = "error.html"
-            }
-          }
-    
-    qa = {
-          acl    = "public-read"
-          bucket = "hl-qa-tutorial"
-          web = {
-              error_document = "index.html"
-              index_document = "error.html"
-            }
-          }
-    stage = {
-          acl    = "private"
-          bucket = "hl-stage-tutorial"
-          web = {
-              error_document = "index.html"
-              index_document = "error.html"
-            }
-          }
-    prod = {
-          acl    = "private"
-          bucket = "hl-prod-tutorial"
-          web = {
-              error_document = "index.html"
-              index_document = "error.html"
-            }
-          }
+      sandbox = {
+        acl    = "private"
+        bucket = "hl-sandbox-tutorial"
+        web = {
+          error_document = "index.html"
+          index_document = "error.html"
+        }
+      }
+
+      qa = {
+        acl    = "public-read"
+        bucket = "hl-qa-tutorial"
+        web = {
+          error_document = "index.html"
+          index_document = "error.html"
+        }
+      }
+      stage = {
+        acl    = "private"
+        bucket = "hl-stage-tutorial"
+        web = {
+          error_document = "index.html"
+          index_document = "error.html"
+        }
+      }
+      prod = {
+        acl    = "private"
+        bucket = "hl-prod-tutorial"
+        web = {
+          error_document = "index.html"
+          index_document = "error.html"
+        }
+      }
     }
   }
 }
